cb0f525055fe33843c07e2cbd2c303f9358ea5e1
jhapuja984.67@gmail.com <jhapuja984.67@gmail.com>
2021-07-17 12:20:28 +0530

    first commit

---
+++ quiz.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+ <meta charset=UTF-8" />
+ 
+ <title>PHP QUIZ | Webdevtrick.com</title>
+ 
+ <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+ 
+<body>
+ 
+ <div id="page-wrap">
+ 
+ <h1>Simple Quiz App using PHP</h1>
+ 
+ <form action="result.php" method="post" id="quiz">
+ 
+            <ol>
+            
+                <li>
+                
+                    <h3>1) PHP stands for -</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="a" />
+                        <label for="question-1-answers-A">a. Hypertext Preprocessor </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="b" />
+                        <label for="question-1-answers-B">b. Pretext Hypertext Preprocessor</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="c" />
+                        <label for="question-1-answers-C">c. personal Home Processor</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-D" value="d" />
+                        <label for="question-1-answers-D">D) Other</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>2) Who is known as the father of PHP?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-A" value="a" />
+                        <label for="question-2-answers-A">a. Drek Kolkevi</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-B" value="b" />
+                        <label for="question-2-answers-B">b. List Barely </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-C" value="c" />
+                        <label for="question-2-answers-C">c. Rasmus Lerdrof
+</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-D" value="d" />
+                        <label for="question-2-answers-D">d. None of the above</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>3) PHP is a....</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-A" value="a" />
+                        <label for="question-3-answers-A">a. Server Side Script</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-B" value="b" />
+                        <label for="question-3-answers-B">b. Programming Language</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-C" value="c" />
+                        <label for="question-3-answers-C">c. Markup Language</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-D" value="d" />
+                        <label for="question-3-answers-D">d. None Of Above These</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>4) Localhost IP is..</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-A" value="a" />
+                        <label for="question-4-answers-A">a. 192.168.0.1</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-B" value="b" />
+                        <label for="question-4-answers-B">b. 127.0.0.0</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-C" value="c" />
+                        <label for="question-4-answers-C">c. 1080:80</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-D" value="d" />
+                        <label for="question-4-answers-D">d. Any Other</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>W5) ebdevtrick Is For</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-A" value="a" />
+                        <label for="question-5-answers-A">a. Web Designer</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-B" value="b" />
+                        <label for="question-5-answers-B">b.  Web Developer</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-C" value="c" />
+                        <label for="question-5-answers-C">c. Graphic Designer</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-D" value="d" />
+                        <label for="question-5-answers-D">d. All Above These</label>
+                    </div>
+                
+                </li>
+            
+            </ol>
+            
+            <input type="submit" value="Submit" class="submitbtn" />
+ 
+ </form>
+ 
+ </div>
+ 
+ 
+</body>
+ 
+</html>
